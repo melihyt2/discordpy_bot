994d7206832cdc9e64df735b46acecaa365a1fa7
melihyt2 <87147139+melihyt2@users.noreply.github.com>
2024-05-14 11:51:52 +0300

    Create ayarlar.py

---
+++ ayarlar.py
@@ -0,0 +1,12 @@
+ayarlar = {
+    'token': 'tokeniniz',
+    'prefix': '!',
+    'durum': 'pirefixm !',
+    'durumlar': [
+        'yardım için !yardım',
+        'oylama başlatmak için !oylamabaşlat',
+        'merhaba kulanıcı',
+        'istatiskilerimi görmek için !istatistik',
+        'sürekli olarak günceleniyorum'
+    ]
+}
